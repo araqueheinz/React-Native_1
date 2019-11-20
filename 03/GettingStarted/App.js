b93018feeec81090f28b0a708d82fed0529fccd4
Heinz Araque <araqueheinz@gmail.com>
2019-11-20 08:34:54 -0500

    added a component

--- 03/GettingStarted/App.js
+++ 03/GettingStarted/App.js
@@ -4,14 +4,17 @@ import React from 'react';
 // Import Reactive Native View & Text Components
 import { View, Text, StyleSheet } from 'react-native';
 
+// Import our NameText component
+import NameText from './src/components/NameText';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textStyle}>
-          This is my custom application
-        </Text>
+        <NameText/>
+        <NameText/>
+        <NameText/>
       </View>
     )
   }
@@ -20,13 +23,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000000",
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center" 
   },
-  textStyle: {
-    fontSize: 22,
-    color: "blue"
-  }
 });
 
---
+++ 03/GettingStarted/src/components/NameText.js
@@ -0,0 +1,30 @@
+// Import the React Library
+import React, { Component } from 'react';
+
+// Import Reactive Native View & Text Components
+import { Text, StyleSheet } from 'react-native';
+
+
+export default class NameText extends Component {
+  render() {
+    return (
+      <Text style={styles.textStyle}>
+        Heinz Araque
+      </Text>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 22,
+    color: "black",
+    backgroundColor: "yellow",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 15,
+    overflow: "hidden",
+    marginBottom: 15,
+  }
+});
+
