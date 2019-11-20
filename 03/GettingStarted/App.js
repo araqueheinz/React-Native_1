917a376ba250e8f931b8599525a58a67d27f768f
Heinz Araque <araqueheinz@gmail.com>
2019-11-20 08:10:17 -0500

    Delete App.js content, start from scratch and added some tyling

--- 03/GettingStarted/App.js
+++ 03/GettingStarted/App.js
@@ -1,19 +1,32 @@
+// Import the React Library
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// Import Reactive Native View & Text Components
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textStyle}>
+          This is my custom application
+        </Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center" 
   },
+  textStyle: {
+    fontSize: 22,
+    color: "blue"
+  }
 });
+
